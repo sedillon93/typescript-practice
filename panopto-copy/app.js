e7b71a14bde47768dab7ac4aff6360852f0e14be
Shannon Dillon <sedillon93@gmail.com>
2018-03-20 13:08:18 -0700

    correctly resetting id of cloned video-template element

--- panopto-copy/app.js
+++ panopto-copy/app.js
@@ -1,12 +1,20 @@
 'use strict';
 var logo = document.getElementById('panopto-logo');
 logo.addEventListener("click", function () {
-    console.log('it worked');
+    console.log('hello');
 });
 var VideoInset = /** @class */ (function () {
-    function VideoInset(image, label) {
+    function VideoInset(image, label, datePublished) {
         this.image = image;
         this.label = label;
+        this.datePublished = datePublished;
     }
     return VideoInset;
 }());
+// function to fill template and append to page using vanilla TypeScript
+var fillTemplate = function () {
+    var videoTemplateNode = document.getElementById('video-template');
+    var newVideo = videoTemplateNode.cloneNode(true);
+    newVideo.setAttribute('id', 'video');
+    console.log(newVideo);
+};
--- panopto-copy/app.ts
+++ panopto-copy/app.ts
@@ -1,15 +1,26 @@
 'use strict';
 
-let logo = document.getElementById('panopto-logo');
+let logo: HTMLElement = document.getElementById('panopto-logo');
 logo.addEventListener("click", function(){
-    console.log('it worked');
+    console.log('hello');
 })
 
 class VideoInset {
     image: string;
     label: string;
-    constructor(image: string, label: string){
+    datePublished: string;
+    constructor(image: string, label: string, datePublished: string){
         this.image = image;
         this.label = label;
+        this.datePublished = datePublished;
     }
+}
+
+// function to fill template and append to page using vanilla TypeScript
+const fillTemplate = () => {
+    let videoTemplateNode: HTMLElement = document.getElementById('video-template');
+    let newVideo: HTMLElement = videoTemplateNode.cloneNode(true) as HTMLElement;
+    newVideo.setAttribute('id', 'video');
+    console.log(newVideo);
+    
 }
